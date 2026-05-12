39063140bee43625a1f960fdbe5c458cd15d6e40
JuanRosero2 <juandrosero52@gmail.com>
2026-05-11 21:31:49 -0500

    jenkins6

--- services/circleguard-promotion-service/build.gradle.kts
+++ services/circleguard-promotion-service/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.testcontainers:junit-jupiter:1.19.3")
     testImplementation("org.testcontainers:postgresql:1.19.3")
     testImplementation("org.testcontainers:neo4j:1.19.3")
